9e7961afbc61bc7999ca4361a1ad2a6eca48df7f
nickol <cnickol@gmail.com>
2020-11-18 15:54:26 +0700

    custom container implemented

---
+++ includes/custom_vector_container.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <
+	class T,
+	class A = std::allocator<T>
+>
+
+struct It {
+	std::vector<T, A>& vec_;
+	int pointer_;
+
+	It(std::vector<T, A>& vec) : vec_{ vec }, pointer_{ 0 } {}
+
+	It(std::vector<T, A>& vec, int size) : vec_{ vec }, pointer_{ size } {}
+
+	bool operator!=(const It<T, A>& other) const {
+		return !(*this == other);
+	}
+
+	bool operator==(const It<T, A>& other) const {
+		return pointer_ == other.pointer_;
+	}
+
+	It& operator++() {
+		++pointer_;
+		return *this;
+	}
+
+	T& operator*() const {
+		return vec_.at(pointer_);
+	}
+};
+
+template<
+	class T,
+	class Allocator = std::allocator<T>
+>
+struct CustomVector {
+	std::vector<T, Allocator> vec_;
+
+	CustomVector() {
+		std::vector<T, std::allocator<T>> vec_;
+	};
+
+	CustomVector(Allocator al) : vec_{ al } {
+	};
+
+	void push_back(T item) {
+		vec_.push_back(item);
+	};
+
+	It<T, Allocator> begin() {
+		return It<T, Allocator>(vec_);
+	}
+
+	It<T, Allocator> end() {
+		return It<T, Allocator>(vec_, vec_.size());
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <map>
 
 #include "arena_allocator.h"
+#include "custom_vector_container.h"
 
-template <class T, std::size_t N> using A = arena_allocator<T, N>;
-template <class T, std::size_t N> using Vector = std::vector<T, arena_allocator<T, N>>;
-template <class Key, class T, std::size_t N> using Map =
+template <class T, std::size_t N> using CA = arena_allocator<T, N>; // custom allocator
+template <class T, std::size_t N> using CACustomVector = // custom vector with custom allocator
+CustomVector<T, arena_allocator<T, N>>;
+template <class Key, class T, std::size_t N> using CAMap = // map with custom allocator
 std::map <
 	Key,
 	T,
@@ -21,19 +23,48 @@ unsigned int factorial(unsigned int n) {
 
 int main() {
 	constexpr int container_size = 10;
-	constexpr std::size_t arena_size = 1024;
+	constexpr std::size_t arena_size = 1024; // bytes
 	arena<arena_size> a;
 
 	std::map<int, int> m;
-	Map<int, int, arena_size> m_custom_allocator{
-		A<std::map<int, int>, arena_size>(a) };
+	CAMap<int, int, arena_size> ca_m{
+		CA<std::map<int, int>, arena_size>(a)
+	};
 
+	CustomVector<int, std::allocator<int>> vec;
+	CACustomVector<int, arena_size> ca_vec{
+		CA<std::vector<int>, arena_size>(a)
+	};
+
+	std::cout << "map with std::allocator" << std::endl;
 	for (int i = 0; i < container_size; ++i) {
-		m_custom_allocator[i] = factorial(i);
-		std::cout << sizeof(m_custom_allocator[i]) << std::endl;
+		m[i] = factorial(i);
+	}
+	for (const auto& it : m) {
+		std::cout << it.first << ' ' << it.second << std::endl;
 	}
 
-	for (const auto& it : m_custom_allocator) {
+	std::cout << "map with custom allocator" << std::endl;
+	for (int i = 0; i < container_size; ++i) {
+		ca_m[i] = factorial(i);
+	}
+	for (const auto& it : ca_m) {
 		std::cout << it.first << ' ' << it.second << std::endl;
+	}
+
+	std::cout << "custom vector with std::allocator" << std::endl;
+	for (int i = 0; i < container_size; ++i) {
+		vec.push_back(i);
+	}
+	for (It<int> it = vec.begin(); it != vec.end(); ++it) {
+		std::cout << *it << std::endl;
+	}
+
+	std::cout << "custom vector with custom allocator" << std::endl;
+	for (int i = 0; i < container_size; ++i) {
+		ca_vec.push_back(i);
+	}
+	for (It<int, arena_allocator<int, arena_size>> it = ca_vec.begin(); it != ca_vec.end(); ++it) {
+		std::cout << *it << std::endl;
 	}
 }
